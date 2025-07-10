542511d1460e0111f8dd440bdcb7ee4de38ae4ea
KyungHoon03 <tw21lkh@naver.com>
2025-07-10 23:33:22 +0900

    init

---
+++ src/main/java/com/example/cukllteam3/CuklLteam3Application.java
@@ -0,0 +1,13 @@
+package com.example.cukllteam3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuklLteam3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuklLteam3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cukllteam3/CuklLteam3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cukllteam3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuklLteam3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
