feddc78c36f798824560adb456ec0f0f60e0255c
KyungHoon03 <tw21lkh@naver.com>
2025-07-11 11:10:26 +0900

    test controller

---
+++ src/main/java/com/example/cukllteam3/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.example.cukllteam3.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MainController {
+
+    public class HelloController {
+        @GetMapping("/")
+        public String home() {
+            return "Hello, CI/CD!";
+        }
+    }
+
+}
